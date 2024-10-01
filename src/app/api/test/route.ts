a468e67428c29ff21534769e064c9bf6feeaabec
bipproduction <bip.production.js@gmail.com>
2024-10-01 21:20:02 +0800

    tambahannya

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -4,12 +4,21 @@ import path from "path";
 export async function GET(request: Request) {
   const text = new URL(request.url).searchParams.get("text");
   if (text) {
+    
     const child = spawn("/bin/bash", [
       "-c",
       `npx tsx ${path.resolve(process.cwd(), "x.ts")}`
     ]);
     child.stdout.on("data", (data) => {
       console.log(`stdout: ${data}`);
+    });
+
+    child.stderr.on("data", (data) => {
+      console.log(`stderr: ${data}`);
+    });
+
+    child.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
     });
 
     return new Response(JSON.stringify({ success: true, text }));
